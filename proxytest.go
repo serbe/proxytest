ca1dd8b90b35e9ef6713d1593ce063f2f3551035
Valeriy Kostikov <serbenv@gmail.com>
2016-03-29 19:45:00 +0300

    use headers list

--- proxytest.go
+++ proxytest.go
@@ -2,37 +2,37 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
 )
 
+var headers = []string{
+	"HTTP_VIA",
+	"HTTP_X_FORWARDED_FOR",
+	"HTTP_FORWARDED_FOR",
+	"HTTP_X_FORWARDED",
+	"HTTP_FORWARDED",
+	"HTTP_CLIENT_IP",
+	"HTTP_FORWARDED_FOR_IP",
+	"VIA",
+	"X_FORWARDED_FOR",
+	"FORWARDED_FOR",
+	"X_FORWARDED",
+	"FORWARDED",
+	"CLIENT_IP",
+	"FORWARDED_FOR_IP",
+	"HTTP_PROXY_CONNECTION",
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<p>Hello, %q</p>", html.EscapeString(r.URL.Path))
-		fmt.Fprintf(w, "<p>X-Forwarded-For: %s</p>", r.Header.Get("X-Forwarded-For"))
-		fmt.Fprintf(w, "<p>X-Forwarded-Host: %s</p>", r.Header.Get("X-Forwarded-Host"))
-		fmt.Fprintf(w, "<p>Forwarded: %s</p>", r.Header.Get("Forwarded"))
-		fmt.Fprintf(w, "<p>X-Forwarded-For: %s</p>", r.Header.Get("Via"))
-		fmt.Fprintf(w, "<p>RemoteAddr: %s</p>", r.RemoteAddr)
-	})
-
+        fmt.Fprintf(w, "<p>RemoteAddr: %s</p>", r.RemoteAddr)
+        for _, header := range headers {
+            str := r.Header.Get(header)
+            if str != "" {
+                fmt.Fprintf(w, "<p>X-Forwarded-For: %s</p>", str)
+            }
+        }
+    })
 	log.Fatal(http.ListenAndServe(":8080", nil))
-
-
-// 'HTTP_VIA',
-// 'HTTP_X_FORWARDED_FOR',
-// 'HTTP_FORWARDED_FOR',
-// 'HTTP_X_FORWARDED',
-// 'HTTP_FORWARDED',
-// 'HTTP_CLIENT_IP',
-// 'HTTP_FORWARDED_FOR_IP',
-// 'VIA',
-// 'X_FORWARDED_FOR',
-// 'FORWARDED_FOR',
-// 'X_FORWARDED',
-// 'FORWARDED',
-// 'CLIENT_IP',
-// 'FORWARDED_FOR_IP',
-// 'HTTP_PROXY_CONNECTION'
 }
