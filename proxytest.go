4c823529316e01806093d1ff1abe0c1344ff6dbc
Valera <mkoctuk@gmail.com>
2016-03-29 15:50:34 +0400

    more headers

--- proxytest.go
+++ proxytest.go
@@ -19,4 +19,20 @@ func main() {
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
+
+// 'HTTP_VIA',
+// 'HTTP_X_FORWARDED_FOR',
+// 'HTTP_FORWARDED_FOR',
+// 'HTTP_X_FORWARDED',
+// 'HTTP_FORWARDED',
+// 'HTTP_CLIENT_IP',
+// 'HTTP_FORWARDED_FOR_IP',
+// 'VIA',
+// 'X_FORWARDED_FOR',
+// 'FORWARDED_FOR',
+// 'X_FORWARDED',
+// 'FORWARDED',
+// 'CLIENT_IP',
+// 'FORWARDED_FOR_IP',
+// 'HTTP_PROXY_CONNECTION'
 }
