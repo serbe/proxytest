3861e635fb7f8cbca2005c5209491f859a8f5825
serbe <mkoctuk@gmail.com>
2016-09-18 00:07:27 +0300

    add log

--- proxytest.go
+++ proxytest.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 var headers = []string{
@@ -24,15 +26,51 @@ var headers = []string{
 	"HTTP_PROXY_CONNECTION",
 }
 
+func existsFile(file string) bool {
+	_, err := os.Stat(file)
+	if err == nil {
+		return true
+	}
+	if os.IsNotExist(err) {
+		return createFile(file)
+	}
+	return true
+}
+
+func createFile(file string) bool {
+	_, err := os.Create(file)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func writeLine(line string, filename string) error {
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	w := bufio.NewWriter(file)
+	fmt.Fprintln(w, line)
+	return w.Flush()
+}
+
 func main() {
+	hasFile := existsFile("log.txt")
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "<p>RemoteAddr: %s</p>", r.RemoteAddr)
-        for _, header := range headers {
-            str := r.Header.Get(header)
-            if str != "" {
-                fmt.Fprintf(w, "<p>%s: %s</p>", header, str)
-            }
-        }
-    })
+		fmt.Fprintf(w, "<p>RemoteAddr: %s</p>", r.RemoteAddr)
+		if hasFile {
+			writeLine(r.RemoteAddr, "log.txt")
+		}
+		for _, header := range headers {
+			str := r.Header.Get(header)
+			if str != "" {
+				fmt.Fprintf(w, "<p>%s: %s</p>", header, str)
+			}
+		}
+	})
 	log.Fatal(http.ListenAndServe(":9090", nil))
 }
